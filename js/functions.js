5c55f9a19737b1c01539d7d548d82d9fe4b21fdb
W4IT-Dev <110252354+W4IT-Dev@users.noreply.github.com>
2022-12-04 18:18:59 +0100

    snow only if its december

--- js/functions.js
+++ js/functions.js
@@ -346,71 +346,74 @@ function search() {
 }
 
 function letItSnow() {
-    if (addPart.checked) {
-        var canvas = document.getElementById('snow-canvas');
-        var context = canvas.getContext('2d');
+    let date = new Date();
+    if (date.getMonth() == 11) {
+        if (addPart.checked) {
+            var canvas = document.getElementById('snow-canvas');
+            var context = canvas.getContext('2d');
 
-        var angle = 0;
+            var angle = 0;
 
-        var width = window.innerWidth;
-        var height = window.innerHeight;
-        canvas.width = width;
-        canvas.height = height;
+            var width = window.innerWidth;
+            var height = window.innerHeight;
+            canvas.width = width;
+            canvas.height = height;
 
-        var snowParticles = 50;
-        var flakes = [];
+            var snowParticles = 50;
+            var flakes = [];
 
-        for (var i = 0; i < snowParticles; i++) {
-            flakes.push({
-                x: Math.random() * width,
-                y: Math.random() * height,
-                radius: Math.random() * 2.5,
-                density: Math.random() * snowParticles
-            });
-        }
+            for (var i = 0; i < snowParticles; i++) {
+                flakes.push({
+                    x: Math.random() * width,
+                    y: Math.random() * height,
+                    radius: Math.random() * 2.5,
+                    density: Math.random() * snowParticles
+                });
+            }
 
-        function drawFlakes() {
-            if (addPart.checked) {
-                context.clearRect(0, 0, width, height);
-                context.fillStyle = 'rgb(255, 255, 255)';
-                context.beginPath();
+            function drawFlakes() {
+                if (addPart.checked) {
+                    context.clearRect(0, 0, width, height);
+                    context.fillStyle = 'rgb(255, 255, 255)';
+                    context.beginPath();
 
+                    for (var i = 0; i < snowParticles; i++) {
+                        var f = flakes[i];
+                        context.moveTo(f.x, f.y);
+                        context.arc(f.x, f.y, f.radius, 0, Math.PI * 2, true);
+                    }
+                    context.fill()
+                    updateCanvas();
+                } else {
+                    flakes = [];
+                    context.clearRect(0, 0, width, height);
+                    clearInterval(snowing);
+                }
+            }
+
+            function updateCanvas() {
+                angle += 0.01
                 for (var i = 0; i < snowParticles; i++) {
                     var f = flakes[i];
-                    context.moveTo(f.x, f.y);
-                    context.arc(f.x, f.y, f.radius, 0, Math.PI * 2, true);
-                }
-                context.fill()
-                updateCanvas();
-            } else {
-                flakes = [];
-                context.clearRect(0, 0, width, height);
-                clearInterval(snowing);
-            }
-        }
 
-        function updateCanvas() {
-            angle += 0.01
-            for (var i = 0; i < snowParticles; i++) {
-                var f = flakes[i];
+                    //update the snowflakes coordinates
+                    f.y += Math.cos(angle + f.density) + 1 + f.radius / 2;
+                    f.x += Math.sin(angle) * 2;
 
-                //update the snowflakes coordinates
-                f.y += Math.cos(angle + f.density) + 1 + f.radius / 2;
-                f.x += Math.sin(angle) * 2;
-
-                if (f.x > width + 5 || f.x < -5 || f.y > height) {
-                    if (i % 3 > 0) {
-                        flakes[i] = { x: Math.random() * width, y: -10, radius: f.radius, density: f.density };
-                    } else {
-                        if (Math.sin(angle) > 0) {
-                            flakes[i] = { x: - 5, y: Math.random() * height, radius: f.radius, density: f.density };
+                    if (f.x > width + 5 || f.x < -5 || f.y > height) {
+                        if (i % 3 > 0) {
+                            flakes[i] = { x: Math.random() * width, y: -10, radius: f.radius, density: f.density };
                         } else {
-                            flakes[i] = { x: width + 5, y: Math.random() * height, radius: f.radius, density: f.density };
+                            if (Math.sin(angle) > 0) {
+                                flakes[i] = { x: - 5, y: Math.random() * height, radius: f.radius, density: f.density };
+                            } else {
+                                flakes[i] = { x: width + 5, y: Math.random() * height, radius: f.radius, density: f.density };
+                            }
                         }
                     }
                 }
             }
+            let snowing = setInterval(drawFlakes, 33);
         }
-        let snowing = setInterval(drawFlakes, 33);
     }
 }//Copyright (c) 2022 by Boris Karastanev (https://codepen.io/ns_bob/pen/BoMqqR)
