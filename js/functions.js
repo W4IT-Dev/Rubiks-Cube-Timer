ca4859b07b46c9d844e51ecc1814a9a096e21204
W4IT-Dev <110252354+W4IT-Dev@users.noreply.github.com>
2022-12-04 15:58:15 +0100

    added snow flakes(credits in script) need to fix bug(softkey footer moves when expand scramble)

--- js/functions.js
+++ js/functions.js
@@ -345,3 +345,71 @@ function search() {
         }
     }
 }
+
+function letItSnow() {
+    //create canvas
+    var canvas = document.getElementById('snow-canvas');
+    var context = canvas.getContext('2d');
+
+    // The initial angle 
+    var angle = 0;
+
+    //Get canvas width and height
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    //Set canvas width and height
+    canvas.width = width;
+    canvas.height = height;
+
+    //Create the snow particles
+    var snowParticles = 75;
+    var flakes = [];
+
+    for (var i = 0; i < snowParticles; i++) {
+        flakes.push({
+            x: Math.random() * width,
+            y: Math.random() * height,
+            radius: Math.random() * 3,
+            density: Math.random() * snowParticles
+        });
+    }
+
+    function drawFlakes() {
+        context.clearRect(0, 0, width, height);
+        context.fillStyle = 'rgb(255, 255, 255)';
+        context.beginPath();
+
+        for (var i = 0; i < snowParticles; i++) {
+            var f = flakes[i];
+            context.moveTo(f.x, f.y);
+            context.arc(f.x, f.y, f.radius, 0, Math.PI * 2, true);
+        }
+        context.fill()
+        updateCanvas();
+    }
+
+    function updateCanvas() {
+        angle += 0.01
+        for (var i = 0; i < snowParticles; i++) {
+            var f = flakes[i];
+
+            //update the snowflakes coordinates
+            f.y += Math.cos(angle + f.density) + 1 + f.radius / 2;
+            f.x += Math.sin(angle) * 2;
+
+            if (f.x > width + 5 || f.x < -5 || f.y > height) {
+                if (i % 3 > 0) {
+                    flakes[i] = { x: Math.random() * width, y: -10, radius: f.radius, density: f.density };
+                } else {
+                    if (Math.sin(angle) > 0) {
+                        flakes[i] = { x: - 5, y: Math.random() * height, radius: f.radius, density: f.density };
+                    } else {
+                        flakes[i] = { x: width + 5, y: Math.random() * height, radius: f.radius, density: f.density };
+                    }
+                }
+            }
+        }
+    }
+    setInterval(drawFlakes, 33);
+}//Copyright (c) 2022 by Boris Karastanev (https://codepen.io/ns_bob/pen/BoMqqR)
